2711b8c9ad97304d1e215698eadfce7b19af4718
fifiteen82726 <fifiteen82726@gmail.com>
2015-08-03 14:56:51 +0800

    Initial search. Title only

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    #@books = Book.where(title: params[:keyword])
+    @books = Book.search(params[:keyword])
   end
 
   # GET /books/1
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,13 +2,26 @@ class Book < ActiveRecord::Base
 
 	scope :finished, ->{ where('finished_on IS NOT NULL') }
 	#scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
+	scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
+
 
 	def self.recent
 		where('finished_on>?', 2.days.ago)
 
 	end
-
+ 
 	def finished?
 		finished_on.present?
 	end
+
+	# def self.search(keyword) 
+	# 	if keyword.present?
+	# 		where(title: keyword)
+	# 	else 
+	# 		self.all
+	# 	end
+
+
+		
+	# end
 end
