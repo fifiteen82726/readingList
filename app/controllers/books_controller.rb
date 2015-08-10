986cac448eb967111966d742c7aa2a0004069e2b
fifiteen82726 <fifiteen82726@gmail.com>
2015-08-11 01:54:01 +0800

    add create.js, book created show at top

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,6 +32,7 @@ class BooksController < ApplicationController
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
+        format .js
       else
         format.html { render :new }
         format.json { render json: @book.errors, status: :unprocessable_entity }
@@ -46,6 +47,7 @@ class BooksController < ApplicationController
       if @book.update(book_params)
         format.html { redirect_to @book, notice: 'Book was successfully updated.' }
         format.json { render :show, status: :ok, location: @book }
+
       else
         format.html { render :edit }
         format.json { render json: @book.errors, status: :unprocessable_entity }
